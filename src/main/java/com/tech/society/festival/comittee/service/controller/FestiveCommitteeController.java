7316200eb7214adb1e98f37680dce8053e42b5a4
shivadharmala <shivaprasadreddy.dharmala@gmail.com>
2025-04-17 14:12:13 +0530

    New code base

--- src/main/java/com/tech/society/festival/comittee/service/controller/FestiveCommitteeController.java
+++ src/main/java/com/tech/society/festival/comittee/service/controller/FestiveCommitteeController.java
@@ -3,6 +3,7 @@ package com.tech.society.festival.comittee.service.controller;
 import com.tech.society.festival.comittee.service.models.FestiveCommittee;
 import com.tech.society.festival.comittee.service.services.FestiveCommitteeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,11 @@ public class FestiveCommitteeController {
 
     @Autowired
     private FestiveCommitteeService service;
+
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok("Hello, welcome to Society Festival Committee Service..!!");
+    }
 
     @PostMapping
     public FestiveCommittee create(@RequestBody FestiveCommittee committee) {
